07bb1d0db97faa486c9f0bb3b5236f0608e22058
Samuel Birky <sbirky21@gmail.com>
2019-10-31 11:51:30 -0500

    editing quantity is completely working

---
+++ src/components/item/EditDetails.js
@@ -0,0 +1,82 @@
+import React, {useState} from "react";
+import { Button, Modal } from 'reactstrap';
+
+
+const ItemEditForm = props => {
+
+    const {
+        className
+      } = props;
+
+      const [modal, setModal] = useState(false);
+      const [quantity, setQuantity] = useState("");
+
+
+
+
+      const toggle = () => {
+        setModal(!modal)
+        setQuantity(props.item.quantity)
+
+      }
+
+
+  const handleEdit = (e) => {
+    e.preventDefault()
+    const editQuantity = {
+        quantity: quantity,
+    }
+
+
+    fetch(`http://localhost:8000/items/${props.item.id}`, {
+        method: "PUT",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Token ${localStorage.getItem("foundit_token")}`
+        },
+        body: JSON.stringify(editQuantity)
+
+
+      })
+      .then(() => props.getItemQuantity(props.item.id))
+      .then(() => toggle())
+    }
+
+    return (
+        <>
+            <Button color="success" onClick={toggle}>Edit Quantity</Button>
+            <Modal isOpen={modal} toggle={toggle} className={className}>
+            {/* Edit Quantity Form */}
+            <main style={{ textAlign: "center" }}>
+            <form className="form--login" onSubmit={handleEdit}>
+                <div className="card">
+                <div className="card-body">
+                <h1 className="card-title h3 mb-3 font-weight-normal">Enter in New Quantity</h1>
+                <fieldset className="form-group">
+                    <label htmlFor="quantity">Quantity</label>
+                    <input
+                    type="number"
+                    name= "quantity"
+                    required
+                    className="form-control"
+                    id="quantity"
+                    placeholder="Quantity"
+                    onChange = {e => setQuantity(e.target.value)}
+                    value={quantity}
+                    />
+                </fieldset>
+                <fieldset>
+                <button type="submit">submit</button>
+                </fieldset>
+                </div>
+                </div>
+            </form>
+            </main>
+            </Modal>
+
+        </>
+      );
+    };
+
+    export default ItemEditForm;
